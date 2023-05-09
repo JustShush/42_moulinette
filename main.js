a2efc8f0ec60f445514adcac496c2af5ac1d9089
JustShush <70417913+JustShush@users.noreply.github.com>
2023-05-09 18:11:13 +0100

    Done.

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 const color = require('colors');
 const { Client, GatewayIntentBits, Partials } = require("discord.js");
-const { Guilds, GuildMembers, GuildMessages, GuildPresences, MessageContent } = GatewayIntentBits;
+const { Guilds, GuildMembers, GuildMessages, MessageContent } = GatewayIntentBits;
 const { User, Message, GuildMember } = Partials;
 
 const client = new Client({
-	intents: [Guilds, GuildMembers, GuildMessages, GuildPresences, MessageContent],
-	partials: [User, Message, GuildMember],
+	intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
+	partials: [User, Message, GuildMember ],
 });
+
+client.config = require("./config.json");
 
 client.on('messageCreate', async (message) => {
 
-	if (message.channel.id != "781504409418989568") return; // welcome channel id
 	const content = message.content
 
-	const prefix = ".";
+	const prefix = client.config.Prefix;
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.toLowerCase().slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
+
+	if (message.channel.id != "781504409418989568") return; // welcome channel id
+
 	if(command === 'kinit') { // expecting ".kinit"
 		const piscineux = "782281297925373962"; // piscineux role
 		const role = "876614161168207993"; // 42_student role
 		const user = message.member.user;
-		const login = args[1]; // login of the member
+		const login = args[0]; // login of the member
 		message.delete(); // deletes member msg/command
 		user.roles.remove(piscineux).catch(err => { console.log(err); }); // removes piscineux role to the member
 		user.roles.add(role).catch(err => { console.log(err); }); // adds 42_student role to the member
@@ -32,9 +36,12 @@ client.on('messageCreate', async (message) => {
 
 });
 
-client.guilds.cache.forEach(guild => {
-	console.log(`${guild.name} | ${guild.id} | ${guild.memberCount} members`.brightRed);
+client.on("ready", () => {
+	client.guilds.cache.forEach(guild => {
+		console.log(`${guild.name} | ${guild.id} | ${guild.memberCount} members`.brightRed);
+	})
+
+	console.log(`${client.user.username} bot is online!`.brightMagenta.bold);
 })
 
-console.log(`${client.user.username} bot is online!`.brightMagenta.bold);
-client.login(client.config.TOKEN);
+client.login(client.config.Token);
